b98ea3010cd5d23499441577d9da296f4bc4b450
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-25 09:45:16 -0500

    models and migrations almost done

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-require './app/models/word.rb'
-
-file = File.open('./words')
-
-file.each_line do |word|
-  chomped = word.chomp
-  sorted = chomped.split(//).sort.join
-  Word.create({:word => chomped, :sorted => sorted})
-end
