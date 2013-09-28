7a50491c969d7a3454888c31be36b48be2c39886
Team1 <student@devbootcamp.com>
2013-09-28 17:31:14 -0500

    hooked up the button events to JS/jQ

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
-  
+
   $('#add_question').on('submit', function(event){
     event.preventDefault();
     var question = $('#question').find("input[name='question[]']").last()
     $(question).after('<br> <label for="question">Question</label> <input type="text" name="question[]">')
-  })
+})
+  var addMultipleChoiceQuestion = function() {
+    console.log("MCQuestion");
+  };
+
+  var addShortAnswerQuestion = function() {
+    console.log("SAQuestion");  
+  };
+  //bind to add short question button
+  var shortAnswerButton = $('#btn_short_question');
+  shortAnswerButton.on("click", addShortAnswerQuestion);
+
+  var multipleChoiceButton = $('#btn_mult_question');
+  multipleChoiceButton.on("click", addMultipleChoiceQuestion);
 
 });
 
