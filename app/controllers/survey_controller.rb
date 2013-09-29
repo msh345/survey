47a5347d81758794a623ac9eb1f18ac161b62367
Mark Horowitz <msh345+lists@gmail.com>
2013-09-29 18:40:54 -0500

    fixed controller

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -51,9 +51,9 @@ post '/survey/create' do
 
   mc_questions.each do |question,answers|
     mc_answers = mc_questions["#{question}"][:answer]
+    survey_questions = survey.questions << Question.create(title: question, question_type: "multichoice")
+    last_question = survey_questions.last
     mc_answers.each do |answer|
-      survey_questions = survey.questions << Question.create(title: question, question_type: "multichoice")
-      last_question = survey_questions.last
       last_question.choices << Choice.create(title: answer)
     end
   end
