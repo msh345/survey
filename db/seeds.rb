476d01b0b832dfb64e7bd920187cb098d6ff557c
Mark Horowitz <msh345+lists@gmail.com>
2013-09-29 15:53:18 -0500

    updated choices table with new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,23 @@
 end
 
 150.times do
-	random_string = (0...8).map { (65 + rand(26)).chr }.join
-	User.find(rand(1..50)).surveys << Survey.create(title: Faker::Lorem.words(num = 3, supplemental = false).join(" "), url: random_string)
+  random_string = (0...8).map { (65 + rand(26)).chr }.join
+  User.find(rand(1..50)).surveys << Survey.create(title: Faker::Lorem.words(num = 3, supplemental = false).join(" "), url: random_string)
 end
 
 450.times do
-  Survey.find(rand(1..150)).questions << Question.create(title: Faker::Lorem.sentence(word_count = 4, supplemental = false, random_words_to_add = 6))
+  Survey.find(rand(1..150)).questions << Question.create(title: Faker::Lorem.sentence(word_count = 4, supplemental = false, random_words_to_add = 6), question_type: ["text","multichoice"].sample)
+end
+
+1.upto(150) do |i|
+  Survey.find(i).questions.each do |question|
+    if question.question_type == "multichoice"
+      choices = [Faker::Lorem.words(num = 3, supplemental = false).join(" "),Faker::Lorem.words(num = 3, supplemental = false).join(" "),Faker::Lorem.words(num = 3, supplemental = false).join(" "),Faker::Lorem.words(num = 3, supplemental = false).join(" ")].sample(rand(2..4))
+      choices.each do |choice|
+        Choice.create(title: choice, question_id: question.id)
+      end
+    end
+  end
 end
 
 1000.times do
